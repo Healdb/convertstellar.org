d4984a19de3b1760213da7c09a0d1bce553aebaf
Healdb <Healdb@users.noreply.github.com>
2014-09-06 17:51:31 -0400

    Update android.js

--- app/app/android.js
+++ app/app/android.js
@@ -1,9 +1,7 @@
-if (window.innerWidth && window.innerWidth <= 600) { 
-    $(document).ready(function(){
+$(document).ready(function(){
         $('#header').append('<div class="leftButton"><a href="navigate.html" style="color: #FFFFFF; text-decoration: none;">Menu</a></div>');
     }); 
     function toggleMenu() {
         $('#header ul').toggleClass('hide'); 
         $('#header .leftButton').toggleClass('pressed');
     }
-}
