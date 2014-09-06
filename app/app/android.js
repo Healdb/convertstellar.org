58619bb51be5c39723a2e5bdbc4793388e50292a
Healdb <Healdb@users.noreply.github.com>
2014-09-06 17:10:31 -0400

    Update android.js

--- app/app/android.js
+++ app/app/android.js
@@ -1,7 +1,7 @@
 if (window.innerWidth && window.innerWidth <= 600) { 
     $(document).ready(function(){
         $('#header ul').addClass('hide'); 
-        $('#header').append('<div class="leftButton"><a href="about.html">Menu</a></div>');
+        $('#header').append('<div class="leftButton"><a href="about.html" style="color: #FFFFFF; text-decoration: none;">Menu</a></div>');
     }); 
     function toggleMenu() {
         $('#header ul').toggleClass('hide'); 
