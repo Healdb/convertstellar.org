a998645616421690537f8efae97ca89d7058bd56
Healdb <Healdb@users.noreply.github.com>
2014-09-10 17:31:35 -0400

    Update android.js

--- app/app/android.js
+++ app/app/android.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        $('#header').append('<div class="leftButton"><a href="navigate.html" style="color: #FFFFFF; text-decoration: none;">Menu</a></div>');
+        $('#header').append('<div class="leftButton"><a href="navigate.html" style="color: #007AFF; text-decoration: none;">Menu</a></div>');
     }); 
     function toggleMenu() {
         $('#header ul').toggleClass('hide'); 
